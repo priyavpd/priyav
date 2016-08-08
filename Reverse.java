1c98ce2da6e9ba0df9303873fe4f271dcf0ba3c8
priyavpd <vpriyacse2@gmail.com>
2016-08-07 22:08:49 -0700

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,23 @@
+# priyav
+public class ReverseNumber {
+ 
+        public static void main(String[] args) {
+               
+                //original number
+                int number = 1234;
+                int reversedNumber = 0;
+                int temp = 0;
+               
+                while(number > 0){
+                       
+                  
+                        temp = number%10;
+                       
+                        reversedNumber = reversedNumber * 10 + temp;
+                        number = number/10;
+                         
+                }
+               
+                System.out.println("Reversed Number is: " + reversedNumber);
+        }
+}
