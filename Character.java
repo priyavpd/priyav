8f76a8fe036432f53efb357c20a3cf1a751aff9e
priyavpd <vpriyacse2@gmail.com>
2016-08-07 23:29:14 -0700

    Update and rename Sample.java to Character.java

---
+++ Character.java
@@ -0,0 +1,26 @@
+# priyav
+public class Character
+{
+public static void main(String[] args)
+{
+String str = "Hello World";
+int len = str.length();
+Map<Character, Integer> numChars = new HashMap<Character, Integer>(Math.min(len, 26));
+
+for (int i = 0; i < len; ++i)
+{
+    char charAt = str.charAt(i);
+
+    if (!numChars.containsKey(charAt))
+    {
+        numChars.put(charAt, 1);
+    }
+    else
+    {
+        numChars.put(charAt, numChars.get(charAt) + 1);
+    }
+}
+
+System.out.println(numChars);
+}
+}
